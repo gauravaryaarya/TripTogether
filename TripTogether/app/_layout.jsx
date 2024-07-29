dff0aa2fe09a77bcf3e589ad705d151f3d5abd13
Gaurav Arya <144447573+gauravaryaarya@users.noreply.github.com>
2024-07-29 16:33:27 +0530

    search

--- TripTogether/app/_layout.jsx
+++ TripTogether/app/_layout.jsx
@@ -1,5 +1,8 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import React from "react";
+import {CreateTripContext} from "../context/CreateTripContext";
+import {useState} from "react";
 
 export default function RootLayout() {
 
@@ -9,11 +12,15 @@ export default function RootLayout() {
     'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf')
   });
 
+  const [tripData, setTripData] = useState([]);
+
   return (
+    <CreateTripContext.Provider value={{tripData,setTripData}}>
     <Stack screenOptions={{headerShown:false}}>
       <Stack.Screen name="(tabs)"/>
     
 
     </Stack>
+    </CreateTripContext.Provider>
   );
 }
--- TripTogether/app/create-trip/searchplace.jsx
+++ TripTogether/app/create-trip/searchplace.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { useNavigation } from 'expo-router'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { CreateTripContext } from './../../context/CreateTripContext'
 
 export default function searchplace() {
 const navigation = useNavigation();
-
+const {tripData, setTripData} = useContext(CreateTripContext);
 useEffect(()=>{
   navigation.setOptions({
     headerShown: true,
@@ -14,19 +15,53 @@ useEffect(()=>{
   })
 },[])
 
+useEffect(()=>{
+  console.log(tripData);
+
+}),[tripData]
+
   return (
-    <View>
+    <View style={{
+      padding:25,
+      paddingTop:75,
+      backgroundColor:'white',
+      height:'100%',
+    }}>
+
+      
       <GooglePlacesAutocomplete
-      placeholder='Search'
+      placeholder='Search Place'
+      fetchDetails={true}
       onPress={(data, details = null) => {
         // 'details' is provided when fetchDetails = true
-        console.log(data, details);
+        console.log(data.description);
+        console.log(details?.geometry.location);
+        console.log(details?.photos[0]?.photo_reference);
+        console.log(details?.url);
+        setTripData({ 
+          locationInfo:{
+            name:data.description,
+            coordinates:details?.geometry.location,
+            photoRef:details?.photos[0]?.photo_reference,
+            url:details?.url,
+          }
+        })
       }}
       query={{
-        key: 'YOUR API KEY',
+        key: 'AIzaSyCcdpFFfKcz-nkaGcjUZPN6lLFfjTpDg2w',
         language: 'en',
       }}
+      styles={{
+        textInputContainer:{
+          borderWidth:1,
+          borderRadius:10,
+          marginTop:25,
+        }
+      }}
     />
+    
+
+
     </View>
   )
 }
